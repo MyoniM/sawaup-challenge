e0a7b45c98ff79289b59eb1eaffd3f345065dd93
Yonatan Merkebu <61096394+MyoniM@users.noreply.github.com>
2023-01-05 10:54:53 +0300

    Dev (#3)
    * API get integration with frontend
    
    * lazy loaded course tumbnails, selectable skills, filtered course

--- src/presentation/components/courseCard.tsx
+++ src/presentation/components/courseCard.tsx
@@ -3,9 +3,8 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import IconButton from '@mui/material/IconButton';
 
-import YouTube, { YouTubeProps } from 'react-youtube';
-
 import styles from '@/presentation/styles/courseCard.module.css';
+import YouTubeLazyLoad from './youtubeLazyLoad';
 
 export interface IProps {
   name: string;
@@ -14,25 +13,14 @@ export interface IProps {
 }
 
 export default function CourseCard({ name, url, isFavorite }: IProps) {
-  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
-    event.target.pauseVideo();
-  };
-
-  const opts: YouTubeProps['opts'] = {
-    height: '200',
-    width: '275',
-    playerVars: {
-      autoplay: 0,
-    },
-  };
-  const videoId = url.split("?v=")[1]
+  const videoId = url.split('?v=')[1];
   return (
     <div className={styles.cardWrapper}>
       <Card sx={{ maxWidth: 275 }}>
         <IconButton aria-label="add to favorites" className={styles.favIco}>
           ♥
         </IconButton>
-        <YouTube videoId={videoId} opts={opts} onReady={onPlayerReady} />
+        <YouTubeLazyLoad youtubeID={videoId} title={'Asdasfsdf'} />
         <CardHeader title={name} />
       </Card>
     </div>
---
+++ src/presentation/components/youtubeLazyLoad.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import NextImage from 'next/image';
+
+interface IProps {
+  youtubeID: string;
+  title: string;
+}
+
+export default function YouTubeLazyLoad({ youtubeID, title }: IProps) {
+  const [showVideo, setShowVideo] = useState(false);
+
+  return (
+    <div>
+      {showVideo ? (
+        <iframe
+          width={275}
+          height={200}
+          src={`https://www.youtube-nocookie.com/embed/${youtubeID}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={title || 'Youtube video'}
+        />
+      ) : (
+        <button
+          type="button"
+          onClick={() => setShowVideo(true)}
+          style={{ cursor: 'pointer', border: 'none', backgroundColor: 'transparent', padding: 0 }}
+        >
+          <NextImage src={`https://img.youtube.com/vi/${youtubeID}/maxresdefault.jpg`} alt="" loading="lazy" height={200} width={275} />
+          <div style={{ position: 'absolute', top: '50%', left: '50%', fontSize: '100px', transform: 'translate(-50%, -50%)', color: '#6c8ffc' }}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="img"
+              width="1em"
+              height="1em"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 32 32"
+              className=""
+            >
+              <path fill="currentColor" d="M7 28a1 1 0 0 1-1-1V5a1 1 0 0 1 1.482-.876l20 11a1 1 0 0 1 0 1.752l-20 11A1 1 0 0 1 7 28Z" />
+            </svg>
+          </div>
+        </button>
+      )}
+    </div>
+  );
+}
